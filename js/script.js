5f2d5fd9bade187fad384f40aa7fbbe08d951ee5
Linnéa <59868518+xlne@users.noreply.github.com>
2021-10-29 11:58:33 +0200

    Removed comments

--- js/script.js
+++ js/script.js
@@ -1,38 +1,10 @@
-// function normalMode() {
-//     var element = document.body;
-//     element.classList.toggle("dark-mode");
-// }
-
-
-// // Select the button
-// const btn = document.querySelector(".navbar-button");
-
-// // Select the stylesheet <link>
-// const theme = document.querySelector("#theme-link");
-
-// // Listen for a click on the button
-// btn.addEventListener("click", function() {
-  
-//     // If the current URL contains "style.css"
-//   if (theme.getAttribute("href") == "style.css") {
-    
-//     // ... then switch it to "dark-theme.css"
-//     theme.href = "style-normal.css";
-//   // Otherwise...
-//   } else {
-//     // ... switch it to "style.css"
-//     theme.href = "style.css";
-//   }
-// });
-
-
-// function to set a given theme/color-scheme
+// Function to set a given theme/color-scheme
 function setTheme(themeName) {
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
 }
 
-// function to toggle between light and dark theme
+// Function to toggle between normal mode and orange mode
 function toggleTheme() {
    if (localStorage.getItem('theme') === ':root'){
        setTheme('theme-normal');
